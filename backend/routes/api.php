fcb20deeb24b18afca1242ad1f4d75717b5c47bd
JordanLippert <lippertx11@gmail.com>
2026-04-24 19:43:40 -0300

    feat: criando e adicionando rota forgot-password

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login',    [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',           [AuthController::class, 'login']);
+Route::post('/register',        [AuthController::class, 'register']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout',  [AuthController::class, 'logout']);
