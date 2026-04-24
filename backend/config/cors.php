5cb090fc69824430b546117b391bb30c3f8366fd
JordanLippert <lippertx11@gmail.com>
2026-04-24 00:31:34 -0300

    feat: adiciona arquivos de configuração (auth, cors, database, sanctum, etc.)

---
+++ backend/config/cors.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure CORS settings for your application. CORS is
+    | a mechanism that allows restricted resources on a web page to be
+    | requested from another domain outside the domain from which the
+    | first resource was served.
+    |
+    | See: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
